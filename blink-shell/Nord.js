8a6f42dc2f9ed9987ed452cdc58b243d4b8df8b9
hwlearner <42940657+hwlearner@users.noreply.github.com>
2019-02-18 10:08:30 +0800

    Update Nord.js
    Correct the content

--- blink-shell/Nord.js
+++ blink-shell/Nord.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#2e3440", "#3b4252", "#434c5e", "#4c566a", "#d8dee9", "#e5e9f0", "#eceff4", "#8fbcbb", "#88c0d0", "#81a1c1", "#5e81ac", "#bf616a", "#d08770", "#ebcb8b", "#a3be8c", "#b48ead"]);
+t.prefs_.set('color-palette-overrides',["#2e3440", "#bf616a", "#a3be8c", "#ebcb8b", "#5e81ac", "#b48ead", "#8fbcbb", "#d8dee9", "#4c566a", "#bf616a", "#a3be8c", "#ebcb8b", "#81a1c1", "#b48ead", "#88c0d0", "#eceff4"]);
 t.prefs_.set('foreground-color', "#2e3440");
 t.prefs_.set('background-color', "#d8dee9");
 t.prefs_.set('cursor-color', 'rgba(22,175,202,0.5)');
