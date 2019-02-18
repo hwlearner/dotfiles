356bafa7d92bcffccc89f640d54faf0b3b96f60f
hwlearner <42940657+hwlearner@users.noreply.github.com>
2019-02-18 20:01:25 +0800

    Update Node.js
    Reverse background and foreground colors

--- blink-shell/Nord.js
+++ blink-shell/Nord.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides',["#2e3440", "#bf616a", "#a3be8c", "#ebcb8b", "#5e81ac", "#b48ead", "#8fbcbb", "#d8dee9", "#4c566a", "#bf616a", "#a3be8c", "#ebcb8b", "#81a1c1", "#b48ead", "#88c0d0", "#eceff4"]);
-t.prefs_.set('foreground-color', "#2e3440");
-t.prefs_.set('background-color', "#d8dee9");
+t.prefs_.set('foreground-color', "#d8dee9");
+t.prefs_.set('background-color', "#2e3440");
 t.prefs_.set('cursor-color', 'rgba(22,175,202,0.5)');
